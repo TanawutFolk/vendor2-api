030cf99659e70eb2518ee06e2f70d592473b35b0
Tanawut Patrawan <Tanawut.pf@gmail.com>
2026-01-19 15:17:26 +0700

    WIP: Test

--- src/_workspace/services/_find-vendor/FindVendorService.ts
+++ src/_workspace/services/_find-vendor/FindVendorService.ts
@@ -8,11 +8,11 @@ export const FindVendorService = {
         try {
             // Get SQL queries [countSql, dataSql]
             const sqlList = await FindVendorSQL.search(dataItem, sqlWhere)
-            
+
             // Execute count query
             const countResult = (await MySQLExecute.search(sqlList[0])) as RowDataPacket[]
             const totalCount = countResult[0]?.TOTAL_COUNT || 0
-            
+
             // Execute data query
             const resultData = (await MySQLExecute.search(sqlList[1])) as RowDataPacket[]
 
@@ -71,17 +71,19 @@ export const FindVendorService = {
     // Update vendor
     updateVendor: async (dataItem: any) => {
         try {
-            // Update vendor
-            const vendorSql = await FindVendorSQL.updateVendor(dataItem)
-            await MySQLExecute.execute(vendorSql)
+            // Update vendor ONLY if company_name is provided (means vendor update)
+            if (dataItem.company_name !== undefined) {
+                const vendorSql = await FindVendorSQL.updateVendor(dataItem)
+                await MySQLExecute.execute(vendorSql)
+            }
 
-            // Update vendor contact if provided
+            // Update vendor contact if vendor_contact_id is provided
             if (dataItem.vendor_contact_id) {
                 const contactSql = await FindVendorSQL.updateVendorContact(dataItem)
                 await MySQLExecute.execute(contactSql)
             }
 
-            // Update vendor product if provided
+            // Update vendor product if vendor_product_id is provided
             if (dataItem.vendor_product_id) {
                 const productSql = await FindVendorSQL.updateVendorProduct(dataItem)
                 await MySQLExecute.execute(productSql)
